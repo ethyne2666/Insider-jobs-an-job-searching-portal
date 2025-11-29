733aca30a3f3256fbab3dc9566d8140948286e71
ethyne2666 <charankumar2666@gmail.com>
2025-11-29 12:30:29 +0530

    Add Hero component with search functionality and context integration

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,12 +1,23 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props)=>{
 
+    const [searchFilter, setSearchFilter] = useState({
+        title: '',
+        location: '',
+        
+    })
+
+    const [isSearched, setIsSearched] = useState(false);
+
     const value = {
         // Add your context values here
+        searchFilter,setSearchFilter,
+        isSearched,setIsSearched
+    
     }
 
     return (

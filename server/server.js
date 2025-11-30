d9d4f581f4ea5ed612d1bf451b158e247bc6ae15
ethyne2666 <charankumar2666@gmail.com>
2025-11-30 19:58:21 +0530

    spelling corrected

--- server/server.js
+++ server/server.js
@@ -24,7 +24,9 @@ app.use(express.json())
 //Routes
 
 app.get('/',(req,res)=>res.send('API Working'))
-
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
 
 app.post('/webhooks',clerkWebhooks)
 
